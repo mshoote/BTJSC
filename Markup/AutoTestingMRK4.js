e7f3a28ff6a5be1815c7bc70d16af37e6c399308
Graymane <dtobler@unomaha.edu>
2017-04-15 19:57:05 -0500

    Update AutoTestingMRK4.js

--- Markup/AutoTestingMRK4.js
+++ Markup/AutoTestingMRK4.js
@@ -50,6 +50,6 @@ window.onload = function() {
   ]));
   }, 0);
 };
-setTimeout(function(){window.location=window.location;},60000);
-//setTimeout(function(){window.location=window.location;},30000);
+//setTimeout(function(){window.location=window.location;},60000);
+setTimeout(function(){window.location=window.location;},30000);
 //setTimeout(function(){window.location=window.location;},10000);
