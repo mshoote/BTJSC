b1711bfed557b2c901f684f2b7caf178ded4278f
Graymane <dtobler@unomaha.edu>
2017-04-22 11:00:05 -0500

    Update mainApp.js

--- ListViewFrameworks/angular/mainApp.js
+++ ListViewFrameworks/angular/mainApp.js
@@ -1,6 +1,6 @@
 var mainApp = angular.module("mainApp", []);
   mainApp.controller('scheduleController', function($scope, $http) {
-    $http.get('http://rawgit.com/mshoote/BTJSC/master/Markup/Json_data/10Items.json').then(function(res){
+    $http.get('http://rawgit.com/mshoote/BTJSC/master/Markup/Json_data/100Items.json').then(function(res){
       $scope.items = res.data;
   });
 });
